8e66c059823b1f972f5a5b95be5de0b66a136dce
Jakob Lybarger <jakoblybarger911@gmail.com>
2020-10-06 18:16:44 -0500

    User class functionality added

--- Server/src/User.java
+++ Server/src/User.java
@@ -3,6 +3,70 @@ import java.net.*;
 import java.util.*;
 
 public class User extends Thread {
-    Socket socket;
-    Server server;
+    private Socket socket;
+    private Server server;
+    private PrintWriter writer;
+
+    /** User constructor.
+     *  @param socket - The users socket
+     *  @param server - The server the user is connected to
+     */
+    public User(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    /** Main function of the class. lets the user enter 0 to many
+     *  messages and prints them out for other users to see, alerts
+     *  when a user has joined, and lets the user leaves/alerts upon
+     *  leaving.
+     */
+    public void run() {
+        try {
+            // Setting out input and output streams
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            OutputStream out = socket.getOutputStream();
+            writer = new PrintWriter(out);
+
+            printUsers();
+
+            String name = reader.readLine();
+            server.addName(name);
+            server.displayMessage("New user: " + name, this);
+
+            String clientMessage;
+            do {
+                clientMessage = reader.readLine();
+                clientMessage = String.format("(%s): %s", name, clientMessage);
+                server.displayMessage(clientMessage, this);
+            } while (!clientMessage.equals("bye"));
+
+            server.removeUser(name, this);
+            socket.close();
+
+            server.displayMessage(name + " left", this);
+        }
+        catch(IOException e) {
+            System.err.printf("Thread Error: %s", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /** Prints all of the users on the server. */
+    void printUsers() {
+        if(server.active()) {
+            writer.println("Users Online: " + server.getUserNames());
+        }
+        else {
+            writer.println("The server is currently inactive...");
+        }
+    }
+
+    /** Prints the message on users screen sent by another user.
+     *  @param message - The message sent by another user
+     */
+    void sendMessage(String message) {
+        writer.println(message);
+    }
 }
